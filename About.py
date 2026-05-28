f0f67322e1f1e49a030d057700a1bc12e392fde2
payalthakur82787 <payalthakur82787@gmail.com>
2026-05-28 23:11:41 +0530

    Add files via upload
    C:\Users\pt401\OneDrive\Desktop\Final Project\House_Price_Project\pages\Analytics.py

---
+++ About.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("ℹAbout Project")
+
+st.write("""
+This is a House Price Prediction project built using:
+
+- Python
+- Machine Learning
+- Scikit-learn
+- Streamlit
+
+The project predicts house prices based on:
+- Area
+- Bedrooms
+- Bathrooms
+- Location
+""")
